ed613e9a1a26f02ce951790e083c34135a24ae00
David Fries <dfries4@gmail.com>
2020-03-02 17:27:19 -0500

    create dj-dev branch

--- api.py
+++ api.py
@@ -5,24 +5,8 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 db=DigitalDJBackend()
 # app.config['CORS_HEADERS'] = 'Content-Type'
-@app.route('/')
-def hello():
-    # DigitalDJBackend().createtesttable()
-    db.inserttestdata("testuser","test_todo","test_category","1")
-    data=db.selecttestdata() 
-    print(data)
-    return jsonify(data)
-@app.route('/createtodo',methods=['POST','OPTIONS'])
-def createtodo():
-    # if request.method=='POST':
-        # data=jsonify(request.data())
-        # print(data)
-    print(request.form['todo'])
-    db.inserttestdata('default',request.form['todo'],request.form['category'],request.form['completed'])
-    return '200'
-@app.route('/todos',methods=['GET'])
-def gettodos():
-    if request.method=='GET':
-        return jsonify(db.selecttestdata())
+
+
+
 if __name__ == '__main__':
     app.run()
--- backend.py
+++ backend.py
@@ -8,18 +8,18 @@ class DigitalDJBackend():
         except Exception as e:
             print(e)
             print("table exists already")
-    def createtesttable(self):
-        query="create table test(user text, todo text, category text,completed text default 0)"
-        self.db.query(query)
+    # def createtesttable(self):
+    #     query="create table test(user text, todo text, category text,completed text default 0)"
+    #     self.db.query(query)
         
-        # rows=self.db.query("select * from test")
-        # print(rows)
-    def selecttestdata(self):
-        query="select * from test"
-        return self.db.query(query).all(as_dict=True)
-    def inserttestdata(self,user,todo,category,completed):
-        print("User: {} TODO: {} Category: {} Completed {}".format(user,todo,category,completed))
-        query="insert into test(user,todo,category,completed) values(:user,:todo, :category,:completed)"
-        self.db.query(query,user=user,todo=todo,category=category,completed=completed)
+    #     # rows=self.db.query("select * from test")
+    #     # print(rows)
+    # def selecttestdata(self):
+    #     query="select * from test"
+    #     return self.db.query(query).all(as_dict=True)
+    # def inserttestdata(self,user,todo,category,completed):
+    #     print("User: {} TODO: {} Category: {} Completed {}".format(user,todo,category,completed))
+    #     query="insert into test(user,todo,category,completed) values(:user,:todo, :category,:completed)"
+    #     self.db.query(query,user=user,todo=todo,category=category,completed=completed)
         
         
