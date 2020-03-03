9d0dbf69fb3136734b3072da7e7a33a7d290bfe9
David Fries <dfries4@gmail.com>
2020-03-03 01:15:05 -0500

    dynamic join room link

--- digitaldj/src/components/Rooms.vue
+++ digitaldj/src/components/Rooms.vue
@@ -1,13 +1,37 @@
 <template>
-    <b-table :striped="true" :data="data" :columns="columns">
-        <!-- <b-table-column field="join" label="Join" width="40">
+    <div>
+    <table class="container is-fluid table">
+        <thead>
+            <tr>
+            <th>Room Name</th>
+            <th>Listeners</th>
+            <th>Genre</th>
+            <th>Join</th>
+            </tr>
+        </thead>
+        <tbody>
+
+            <tr v-for="row in data" :key="row.id">
+                <td>{{row.room_name}}</td>
+                <td>{{row.listeners}}</td>
+                <td>{{row.genre}}</td>
+                <td><router-link class="button is-warning" :to="{name:'room',params:{id:row.id}}">Join Room</router-link></td>
+            </tr>
+
+        </tbody>
+    </table>
+</div>
+
+
+    <!-- <b-table :striped="true" :data="data" :columns="columns">
+         <b-table-column field="join" label="Join" width="40">
                     <router-link :to="{name:'room',params:data.id}">Join Room</router-link>
                     
-        </b-table-column> -->
+        </b-table-column> 
 
 
-    </b-table>
-</template>
+     </b-table> -->
+</template> 
 
 <script>
 
