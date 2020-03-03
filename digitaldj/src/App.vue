2510825b8f782414fc502486828682f0462a3deb
David Fries <dfries4@gmail.com>
2020-03-02 19:57:17 -0500

    removed init styling

--- digitaldj/src/App.vue
+++ digitaldj/src/App.vue
@@ -55,13 +55,4 @@ $link-focus-border: $primary;
 @import "~buefy/src/scss/buefy";
 </style>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
