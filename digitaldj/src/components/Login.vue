950e14d3d6d55f132f824a330c28ca98313958b4
David Fries <dfries4@gmail.com>
2020-03-05 23:00:15 -0500

    working login modal popup

--- digitaldj/src/components/Login.vue
+++ digitaldj/src/components/Login.vue
@@ -1,7 +1,7 @@
 <template>
     <section>
         <button class="button is-danger"
-            @click="loginmodal()">
+            @click="isComponentModalActive = true">
             Login
         </button>
 
@@ -10,15 +10,7 @@
                  trap-focus
                  aria-role="dialog"
                  aria-modal>
-            <modal-form v-bind="formProps"></modal-form>
-        </b-modal>
-    </section>
-</template>
-
-<script>
-const ModalForm = {
-        props: ['email', 'password'],
-        template: `
+            <!-- <Login v-bind="formProps"></Login> -->
             <form action="">
                 <div class="modal-card" style="width: auto">
                     <header class="modal-card-head">
@@ -47,27 +39,34 @@ const ModalForm = {
                         <b-checkbox>Remember me</b-checkbox>
                     </section>
                     <footer class="modal-card-foot">
-                        <button class="button" type="button" @click="$parent.close()">Close</button>
+                        <!-- <button class="button" type="button" @click="$parent.close()">Close</button> -->
+                        <div class="container has-text-centered">
                         <button class="button is-primary">Login</button>
+
+                        </div>
                     </footer>
                 </div>
             </form>
-        `
-    }
+        </b-modal>
+    </section>
+</template>
+
+<script>
+// const Login = {
+//         props: ['email', 'password'],
+//         template: `
+            
+//         `
+//     }
 export default {
     name:"Login",
-    methods:{
-        loginmodal(){
-            this.$buefy.modal.open({
-                component:ModalForm,
-              hasModalCard:true,
-              trapFocus:true
-            })
-        }
-    },
+    // components:{
+    //     Login
+    // },
     data(){
         return {
             isComponentModalActive: false,
+            showModal:true,
             formProps: {
                 email:'email@email.com',
                 password:'hunter2'
