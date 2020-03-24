55e4f355ae3f9793f381a3e47255846e3b270887
David Fries <dfries4@gmail.com>
2020-03-23 21:50:20 -0400

    master server work, added requirements.txt for python dependencies

--- masterserver.py
+++ masterserver.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 import pickle
+import sys
 class MasterServer():
     def __init__(self):
         self.host="localhost"
@@ -9,15 +10,20 @@ class MasterServer():
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #the SO_REUSEADDR flag tells the kernel to
         self.s.bind((self.host, self.port))
+    def conn(self):
+        print("starting listen thread")
+        self.s.listen(5)
     def listen(self):
         print("listening for connections...")
+        # threading.Thread(target=self.conn()).start()
         self.s.listen(5)
         while True:
+            print("accepting connection")
             c, addr = self.s.accept()
             c.settimeout(60)
             threading.Thread(target = self.receivestream(c)).start()
     def receivestream(self,c):
-        
+        print("receiving data")
         while True:
             
             packet=c.recv(4096)
@@ -27,4 +33,9 @@ class MasterServer():
         print(pickle.loads(b"".join(self.streamdata)))
 
 if __name__ == "__main__":
+    # threading.Thread(target=MasterServer().listen()).start()
     MasterServer().listen()
+    while True:
+        cmd=input("Enter command: ")
+        if(cmd=="exit"):
+            sys.exit()
