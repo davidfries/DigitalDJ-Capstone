60c8afefb0d047cbbe540b6c2eba14a5a8535709
saraashcraft <42613687+saraashcraft@users.noreply.github.com>
2020-04-09 11:52:54 -0400

    fixed sql and password

--- backend.py
+++ backend.py
@@ -69,11 +69,8 @@ class DigitalDJBackend():
 
     #USER METHODS
     def createuser(self,email,password):
-        # get last client key in db and increment
-        client_key = int(self.db.query(
-            "SELECT client_key FROM users ORDER BY client_key DESC LIMIT 1;"
-            ).as_dict()[0].get("client_key")) + 1
-        query="INSERT INTO users (client_key, email, password) VALUES (:client_key,:email,:password)"
+        client_key = self.genid()
+        query="INSERT INTO users (client_key, email, password) VALUES (:client_key,:email,crypt(:password, gen_salt('bf')))"
         try:
             self.db.query(query,client_key=client_key,email=email,password=password)
         except Exception as e:
