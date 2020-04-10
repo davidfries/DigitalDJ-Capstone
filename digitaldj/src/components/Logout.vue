85c2aa017b3f84fb76aa7bfae52533983530b4d6
saraashcraft <42613687+saraashcraft@users.noreply.github.com>
2020-04-10 16:44:10 -0400

    added logout component

---
+++ digitaldj/src/components/Logout.vue
@@ -0,0 +1,21 @@
+<template>
+    <section>
+        <button class="button is-danger"
+            @click="logout">
+            Logout
+        </button>
+    </section>
+</template>
+
+<script>
+export default {
+    name:"Logout",
+    methods:{
+        logout:function(){
+            this.$session.destroy()
+            this.$router.go()
+            this.$router.push({path: 'home'})
+        }
+    }
+}
+</script>
