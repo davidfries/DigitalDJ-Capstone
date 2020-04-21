6d072a982de991a9b2747a8890f4b7c2cb6abaf0
David Fries <dfries4@gmail.com>
2020-04-21 01:03:21 -0400

    args from streamurl please work

--- app/main.py
+++ app/main.py
@@ -64,7 +64,7 @@ def update_songs(data):
 def authstream():
     if request.method=='POST':
         print("Posted from server")
-        print(request.data)
+        print([x for x in request.args.listvalues()])
         return Response("{'msg':'Successful stream join'",status=201,mimetype='application/json')
     elif request.method =='GET':
         print("Get from server")
