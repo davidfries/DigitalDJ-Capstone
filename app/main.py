cc0960ba5b3b7a01cf2213226dd733d698ddf249
David Fries <dfries4@gmail.com>
2020-04-27 23:54:59 -0400

    corrected api paramters for add room

--- app/main.py
+++ app/main.py
@@ -157,7 +157,7 @@ def addroom():
     if request.method=='POST':
         data=request.get_json()
         try:
-            db.createroom(data['room_name'],data['quantity'],data['roomsecurity'],data['genre'])
+            db.createroom(data['room_name'],data['max_listeners'],data['security'],data['genre'])
             return jsonify({"msg":"successful room creation"})
         except Exception as e:
             print(e)
