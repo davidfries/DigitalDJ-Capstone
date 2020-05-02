7aaeb2854a3a8695955fe8da1945da70b3cfd098
saraashcraft <42613687+saraashcraft@users.noreply.github.com>
2020-05-02 14:57:30 -0400

    added username/password change forms

--- digitaldj/src/components/UserSettings.vue
+++ digitaldj/src/components/UserSettings.vue
@@ -9,15 +9,16 @@
                         <p>{{this.$session.get('username')}}<br></p>
                         <b-nav>
                             <b-nav-item class="button pad is-primary"
-                                @click="changeUsername">
+                                @click="isUsernameModalActive = true">
                                 Change Username
                             </b-nav-item>
                             <b-nav-item class="button pad is-primary"
-                                @click="changePassword">
+                                @click="isPasswordModalActive = true">
                                 Change Password
                             </b-nav-item>
                         </b-nav>
             </div>
+            <br>
             <div class="pad card flex-container">
                 <h1>My Rooms<br></h1>
                 <table class="container table">
@@ -30,7 +31,95 @@
 
                     </tbody>
                     </table>
+                    <br>
             </div>
+
+            <b-modal :active.sync="isUsernameModalActive"
+                 has-modal-card
+                 trap-focus
+                 aria-role="dialog"
+                 aria-modal>
+                <form action="" @submit.prevent="changeUsername">
+                    <div class="modal-card" style="width: auto">
+                        <header class="modal-card-head">
+                            <p class="modal-card-title">Change Username</p>
+                        </header>
+                        <section class="modal-card-body">
+                        <b-field label="New Username">
+                            <b-input
+                                v-model="username"
+                                type="username"
+                                :value="username"
+                                placeholder="New username"
+                                required>
+                            </b-input>
+                        </b-field>
+                    </section>
+                    <footer class="modal-card-foot">
+                        <!-- <button class="button" type="button" @click="$parent.close()">Close</button> -->
+                        <div class="container has-text-centered">
+                        <button class="button is-primary" @click="isUsernameModalActive=false">Change</button>
+
+                        </div>
+                    </footer>
+                    </div>
+                </form>
+            </b-modal>
+
+            <b-modal :active.sync="isPasswordModalActive"
+                 has-modal-card
+                 trap-focus
+                 aria-role="dialog"
+                 aria-modal>
+                <form action="" @submit.prevent="changePassword">
+                    <div class="modal-card" style="width: auto">
+                        <header class="modal-card-head">
+                            <p class="modal-card-title">Change Password</p>
+                        </header>
+                        <section class="modal-card-body">
+                        <b-field label="Old Password">
+                            <b-input
+                                v-model="password"
+                                type="password"
+                                :value="password"
+                                password-reveal
+                                placeholder="Your old password"
+                                required>
+                            </b-input>
+                        </b-field>
+
+                        <b-field label="New Password">
+                            <b-input
+                                v-model="newPassword"
+                                type="newPassword"
+                                :value="newPassword"
+                                password-reveal
+                                placeholder="Your new password"
+                                required>
+                            </b-input>
+                        </b-field>
+
+                        <b-field label="New Password Validation">
+                            <b-input
+                                v-model="newPasswordValid"
+                                type="newPasswordValid"
+                                :value="newPasswordValid"
+                                password-reveal
+                                placeholder="Retype your new password"
+                                required>
+                            </b-input>
+                        </b-field>
+                    </section>
+                    <footer class="modal-card-foot">
+                        <!-- <button class="button" type="button" @click="$parent.close()">Close</button> -->
+                        <div class="container has-text-centered">
+                        <button class="button is-primary" @click="isPasswordModalActive=false">Change</button>
+
+                        </div>
+                    </footer>
+                    </div>
+                </form>
+            </b-modal>
         </div>
     </section>
 </template>
@@ -41,8 +130,12 @@ export default {
     name:"UserSettings",
     data(){
         return{
+            isUsernameModalActive: false,
+            isPasswordModalActive: false,
             email: "",
             password: "",
+            newPassword: "",
+            newPasswordValid: "",
             username: "",
             rooms: []
         }
