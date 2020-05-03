e36d6ba8ef94c2b437478dc8cda4a2885ad0352e
David Fries <dfries4@gmail.com>
2020-05-03 18:14:50 -0400

    removed allow_origins_param

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ import json
 from backend import DigitalDJBackend
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "https://api.digitaldj.live"}},supports_credentials=True).init_app(app)
-socketio = SocketIO(app,cors_allowed_origins='*')
+socketio = SocketIO(app)
 db=DigitalDJBackend()
 app.config['CORS_HEADERS'] = 'Content-Type'
 
